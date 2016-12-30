2090ee9b1f1a45b7379f27639e8ec6653809ea11
Niaz ali <niazboghri@gmail.com>
2016-12-30 23:30:46 +0500

    simple website y-Z traders half complete

---
+++ readme.md
@@ -0,0 +1,4 @@
+# simple Website 
+1 - it is the good making experience with it website to make ..
+## *Y-Z TRADERS*
+here is the link of the web [Y-z Traders](https://salery-calculator.firebaseapp.com/) .
---
+++ script.js
@@ -0,0 +1,17 @@
+//details div inner content heree
+	function yarn1(){	
+	var categories = document.getElementById("elastic").innerHTML = "<ol><li>100/100/140 R/white</li><li>100/100/140 Black</li><li>Elastic nylon 140D R/white</li> <li>elastic nylon 140D Black</li></ol>";
+	}
+	function yarn2(){	
+	var categories = document.getElementById("polyster").innerHTML = "<ol><li>150/48/0 White</li><li>150/48/0 Black</li><li>75/36/0 white</li> <li>75/36/0 Black</li><li>75/36 intermengal</ol>";
+	}
+	function nylon3(){
+		var categories = document.getElementById("nylon").innerHTML = "<ol><li>70/24/2 nylon R/white</li><li>70/24/2 nylon Black</li></ol>"
+	}
+	function yarn4(){
+		var categories = document.getElementById("spandex").innerHTML = "<ol><li>20 D Spandex</li></ol>"
+	}
+	function yarn5(){
+		var categories = document.getElementById("lycra").innerHTML = "<ol><li>lycra yarn white</li><li>lycra yarn Black</li></ol>"
+	}
+//end of the inner content function of deatils yarn categories .............
